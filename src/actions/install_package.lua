fcb4865c2f421b02e538736d4b5623748bb15d89
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-08-13 16:15:41 -0300

    at

--- src/actions/install_package.lua
+++ src/actions/install_package.lua
@@ -5,5 +5,9 @@ function  Install_package()
         print(private_vibescript.RED.."No package name provided"..private_vibescript.RESET)
         return 1
     end
+    local package_dir = get_prop("pushblind.package_dir."..name)
+    local filename = get_prop("pushblind.package_file."..name)
+    local full_file_path = package_dir.."/"..filename
+    print(full_file_path)
     
 end
