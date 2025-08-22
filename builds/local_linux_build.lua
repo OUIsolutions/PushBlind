db5d6c25ff06e7d07ebb95469462ea03d1c93d57
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-08-22 04:11:37 -0300

    att

--- builds/local_linux_build.lua
+++ builds/local_linux_build.lua
@@ -1,6 +1,6 @@
 
-function local_linux_build()
-    extension_build()
+function local_unux_compilation()
+
     local comand = [[gcc  dependencies/vibescript.c -DCONTENT_ENCRYPT_KEY=\"../keys/content.h\" -DLLM_ENCRYPT_KEY=\"../keys/llm.h\" -DNAME_ENCRYPT_KEY=\"../keys/name.h\"  -DVIBE_EXTENSION_MODULE=\"../release/extension.c\" -DVIBE_EXTENSION_FUNC=ouivibe -DVIBE_EXTENSION_LIB_NAME=\"ouivibe\"   -o pushblind]]
     os.execute(comand)
    
