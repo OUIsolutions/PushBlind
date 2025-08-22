7b5caee4c7d286acea1b9088e249b41fdc7ff705
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-08-22 03:53:56 -0300

    att

---
+++ darwinconf.lua
@@ -0,0 +1,13 @@
+PROJECT_NAME = "pushblind"
+CONTANIZER   =  darwin.argv.get_flag_arg_by_index({ "contanizer", }, 1,"docker" ) 
+VERSION      = "0.0.1"
+LICENSE      = "MIT"
+URL          = "https://github.com/OUIsolutions/Ai-RagTemplate"
+DESCRIPITION = "A Runtime to work with llms"
+FULLNAME     = "VibeScript"
+EMAIL        = "mateusmoutinho01@gmail.com"
+SUMARY       = "A Runtime to work with llms"
+YOUR_CHANGES = "--"
+
+LAUNGUAGE     = "c"
+darwin.load_all("builds")
