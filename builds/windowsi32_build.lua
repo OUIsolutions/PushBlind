df9e01be93317026e86917f07c7572cac9f85a77
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-08-24 02:33:45 -0300

    att

--- builds/windowsi32_build.lua
+++ builds/windowsi32_build.lua
@@ -19,14 +19,14 @@ function windowsi32_build()
             { "././dependencies", "/dependencies" },
 
         },
-        command = compiler..[[ --static /dependencies/vibescript.c  -DCONTENT_ENCRYPT_KEY=\\"../keys/content.h\\" -DLLM_ENCRYPT_KEY=\\"../keys/llm.h\\" -DNAME_ENCRYPT_KEY=\\"../keys/name.h\\"   -DDEFINE_DEPENDENCIES  -DVIBE_EXTENSION_MODULE=\\"/release/extension.c\\" -DVIBE_EXTENSION_FUNC=pushblind  -DVIBE_EXTENSION_LIB_NAME=\\"pushblind\\" -lws2_32  -o /release/pushblindi32.exe ]]
+        command = compiler..[[ --static /dependencies/vibescript.c  -DCONTENT_ENCRYPT_KEY=\\"../keys/content.h\\" -DLLM_ENCRYPT_KEY=\\"../keys/llm.h\\" -DNAME_ENCRYPT_KEY=\\"../keys/name.h\\"   -DDEFINE_DEPENDENCIES  -DVIBE_EXTENSION_MODULE=\\"/release/pushblind_extension.c\\" -DVIBE_EXTENSION_FUNC=pushblind  -DVIBE_EXTENSION_LIB_NAME=\\"pushblind\\" -lws2_32  -o /release/pushblindi32.exe ]]
 
     })
 end
 
 darwin.add_recipe({
     name=".exe",
-    requires={"linux_bin"},
+    requires={"extension"},
     description="make a .exe of the project",
     outs={"release/ouivibei32.exe"},
     callback=windowsi32_build
