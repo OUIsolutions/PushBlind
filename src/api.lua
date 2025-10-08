afbea0373b5494d25b30f4da2ec4a7c7efca2633
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-10-07 22:20:24 -0300

    att

--- src/api.lua
+++ src/api.lua
@@ -88,9 +88,10 @@ function PushBlind.run_action(name, action_name,args)
     PushBlind.repo_dir = repo_dir
     PushBlind.same  = PushBlind.repo_dir
     local absolute_repo_dir = pushblind_repos_dir..repo_dir
-    script_dir_name = absolute_repo_dir
     local filename = dtw.load_file(package_info_dir.."/filename.txt")
     local filename_path = absolute_repo_dir.."/"..filename
+    script_dir_name = dtw.newPath(filename).get_dir()
+
     local ok, error = os.execute("cd "..absolute_repo_dir.." && "..PUSH_BLIND_PULL_COMMAND)
     if not ok then
         return false,"impossible to execute command: "..PUSH_BLIND_PULL_COMMAND
